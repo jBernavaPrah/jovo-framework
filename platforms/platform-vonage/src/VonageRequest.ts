ae0b11d7bdc36ba217b2830f474346a67cc78f88
jbernava <jure.bernava@scalapay.com>
2023-07-03 15:11:07 +0200

    :sparkles: Added Vonage Platform to Jovo framework

---
+++ platforms/platform-vonage/src/VonageRequest.ts
@@ -0,0 +1,104 @@
+import { CapabilityType, InputType, JovoInput, JovoRequest } from '@jovotech/framework';
+import { InputTypeLike, UnknownObject } from '@jovotech/cli-core';
+
+export class VonageRequest extends JovoRequest {
+    $type? = 'vonage';
+
+    $eventUrl = '';
+
+    locale?: string;
+    to!: string;
+    from!: string;
+    uuid!: string;
+    timestamp!: string;
+    conversation_uuid!: string;
+    direction?: 'inbound' | 'outbound';
+    dtmf?: {
+        digits: string;
+        time_out: boolean;
+    };
+    speech?: {
+        timeout_reason?: 'end_on_silence_timeout' | 'max_duration' | 'start_timeout';
+        error?: string;
+        recording_url?: string;
+        results?: {
+            confidence: number;
+            text: string;
+        }[];
+    };
+
+    getDeviceCapabilities(): CapabilityType[] | undefined {
+        return [];
+    }
+
+    getEntities(): JovoInput['entities'] {
+        return;
+    }
+
+    getInputAudio(): JovoInput['audio'] {
+        return;
+    }
+
+    getInputText(): JovoInput['text'] {
+        return (
+            this.speech?.results?.filter((r) => r.confidence > 0.5)[0]?.text ?? this.dtmf?.digits
+        );
+    }
+
+    getInputType(): InputTypeLike | 'DTMF' | undefined {
+        if (!this.speech && !this.dtmf) return InputType.Launch;
+        if (this.dtmf?.digits) return 'DTMF';
+        return InputType.Text;
+    }
+
+    getIntent(): JovoInput['intent'] {
+        if (this.speech?.error) return 'error';
+
+        return this.speech?.timeout_reason &&
+            this.speech?.timeout_reason !== 'end_on_silence_timeout'
+            ? 'silence'
+            : undefined;
+    }
+
+    getLocale(): string | undefined {
+        // search for the locale from call?
+
+        return this.locale;
+    }
+
+    getSessionData(): UnknownObject | undefined {
+        return;
+    }
+
+    getSessionId(): string | undefined {
+        return this.conversation_uuid;
+    }
+
+    getUserId(): string | undefined {
+        return this.from;
+    }
+
+    isNewSession(): boolean | undefined {
+        return false;
+    }
+
+    setIntent(intent: string): void {
+        this.nlu = { intentName: intent };
+    }
+
+    setLocale(locale: string): void {
+        this.locale = locale;
+    }
+
+    setSessionData(data: Record<string, unknown>): void {
+        return;
+    }
+
+    setUserId(): void {
+        return;
+    }
+
+    getRequestId(): string | undefined {
+        return this.uuid;
+    }
+}
---
+++ platforms/platform-vonage/src/output/utilities.ts
@@ -0,0 +1,47 @@
+import { MessageValue } from '@jovotech/framework';
+import { ActionAction, InputAction, InputType, TalkAction } from './actions';
+import { plainToInstance } from 'class-transformer';
+
+export function convertMessageToVonageTalk(
+    message: MessageValue,
+    config: Omit<TalkAction, 'action' | 'text'> = {},
+): TalkAction {
+    return createTalkAction(
+        typeof message === 'string' ? message : message.text || (message.speech as string),
+        config,
+    );
+}
+
+export function createTalkAction(
+    text: string,
+    config: Omit<TalkAction, 'action' | 'text'> = {},
+): TalkAction {
+    return plainToInstance(TalkAction, {
+        action: ActionAction.Talk,
+        text,
+        ...config,
+    });
+}
+
+export function createSpeechInputAction(
+    config: Omit<InputAction, 'action' | 'type' | 'dtmf'> = {},
+): InputAction {
+    return createInputAction([InputType.speech], config);
+}
+
+export function createDTMFInputAction(
+    config: Omit<InputAction, 'action' | 'type' | 'speech'> = {},
+): InputAction {
+    return createInputAction([InputType.dtmf], config);
+}
+
+export function createInputAction(
+    type: InputType[],
+    config: Partial<InputAction> = {},
+): InputAction {
+    return plainToInstance(InputAction, {
+        action: ActionAction.Input,
+        type,
+        ...config,
+    });
+}
